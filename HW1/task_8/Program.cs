1753a617553c8571e5b7be5d35c0276894906859
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-20 23:36:47 +0300

    Added a solution for task_8

---
+++ HW1/task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+System.Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 1) {
+    System.Console.WriteLine("Число N должно быть больше 1");
+    return;
+}
+
+int count = 1;
+while (count <= N) {
+    if (count % 2 == 0) System.Console.WriteLine(count);
+    count++;
+}
