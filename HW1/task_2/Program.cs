f079db92c37a4c0c726a620c725ff93b0e11bfff
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-20 23:13:18 +0300

    Added a solution for task_2

---
+++ HW1/task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.Write("Enter first number: ");
+int num_1 = int.Parse(Console.ReadLine());
+System.Console.Write("Enter second number: ");
+int num_2 = int.Parse(Console.ReadLine());
+
+if (num_1 > num_2) {
+    System.Console.WriteLine($"Max number is {num_1}");
+} else {
+    System.Console.WriteLine($"Max number is {num_2}");
+}
