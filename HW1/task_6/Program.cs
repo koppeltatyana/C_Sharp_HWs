880e79bb449d0eb8fb6db9502ffae3e09535f9d9
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-20 23:28:32 +0300

    Added a solution for task_6

---
+++ HW1/task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0) {
+    System.Console.WriteLine($"Число {num} делится на 2 без остатка");
+} else {
+    System.Console.WriteLine($"Число {num} делится на 2 с остатком");
+}
