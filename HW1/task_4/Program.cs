f7611d96f744d2132b1116d1d5be9641de350d2f
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-20 23:20:41 +0300

    Added a solution for task_4

---
+++ HW1/task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+System.Console.Write("Enter first number: ");
+int num_1 = int.Parse(Console.ReadLine());
+System.Console.Write("Enter second number: ");
+int num_2 = int.Parse(Console.ReadLine());
+System.Console.Write("Enter third number: ");
+int num_3 = int.Parse(Console.ReadLine());
+
+if (num_1 > num_2 && num_1 > num_3) {
+    System.Console.WriteLine($"Max number is {num_1}");
+} else if (num_2 > num_1 && num_2 > num_3) {
+    System.Console.WriteLine($"Max number is {num_2}");
+} else if (num_3 > num_1 && num_3 > num_2) {
+    System.Console.WriteLine($"Max number is {num_3}");
+}
