0340acca57bc5b5a5bd390478e54b70c15090277
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-23 22:20:21 +0300

    Added some more fixes

--- HW2/task_1/Program.cs
+++ HW2/task_1/Program.cs
@@ -9,7 +9,7 @@ while (true) {
     int num = int.Parse(Console.ReadLine());
     if (Math.Abs(num) > 999 || Math.Abs(num) < 100) System.Console.WriteLine("Число должно быть трехзначным!");
     else {
-        System.Console.WriteLine($"Вторая цифра числа {num} -> {num / 10 % 10}");
+        System.Console.WriteLine($"Вторая цифра числа {num} -> {Math.Abs(num / 10 % 10)}");
         break;
     }
 }
