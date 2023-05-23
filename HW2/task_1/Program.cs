07613e27c61e438ca8310285beb84b434ad124fc
koppel_tatyana <tatyana.koppel@mail.ru>
2023-05-23 21:42:24 +0300

    Added a resolution for task1

---
+++ HW2/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+while (true) {
+    System.Console.Write("Введите трехзначное число: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num > 999 || num < -999) System.Console.WriteLine("Число должно быть трехзначным!");
+    else {
+        System.Console.WriteLine($"Вторая цифра числа {num} -> {num / 10 % 10}");
+        break;
+    }
+}
