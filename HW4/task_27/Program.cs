7ec6dc6b3420328b2da7ba0287e0d30d07d87400
koppel_tatyana <tatyana.koppel@mail.ru>
2023-06-01 09:30:43 +0300

    Added solution for task_27

---
+++ HW4/task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumsSum(int num) {
+    int nums_sum = 0;
+    while (num > 0) {
+        nums_sum += num % 10;
+        num /= 10;
+    }
+    return nums_sum;
+}
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int num = int.Parse(System.Console.ReadLine());
+System.Console.WriteLine($"Сумма цифр числа {num} равна {GetNumsSum(num)}");
+
