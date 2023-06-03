ea619febf130d52256e2f0aa1ead051975704bbe
koppel_tatyana <tatyana.koppel@mail.ru>
2023-06-03 17:06:09 +0300

    Added solution for task_38

---
+++ HW5/task_38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+Console.Clear();
+System.Console.Write("Введите размерность массива: ");
+int arr_size = int.Parse(System.Console.ReadLine());
+
+double[] random_array = GetRandomDoubleArray(array_size: arr_size);
+System.Console.WriteLine($"Массив случайных вещественных чисел длиной {arr_size}: [{String.Join(", ", random_array)}]");
+PrintEmptyString();
+double[] array_max_min = GetArrayMaxMin(array: random_array);
+System.Console.WriteLine(
+    $"Разница между максимальным ({array_max_min[0]}) и минимальным ({array_max_min[1]}) значениями равно {array_max_min[0] - array_max_min[1]}");
+
+/*----------------------- РЕАЛИЗАЦИЯ МЕТОДОВ -----------------------*/
+double[] GetRandomDoubleArray(int array_size) {
+    double[] result = new double[array_size];
+
+    int[] num_sign = {-1, 1};
+    for (int i = 0; i < array_size; i++) {
+        result[i] = 
+        Math.Round(
+            new Random().NextDouble() * new Random().Next(1, 20) * num_sign[new Random().Next(0, 2)], 3
+        ); // домножаем на произвольное число, так как NextDouble генерирует число от 0 до 1
+    }
+    return result;
+}
+
+double[] GetArrayMaxMin(double[] array) {
+    double[] result = new double[2];
+    result[0] = array[0];  // первое число будет max
+    result[1] = array[0];  // второе число будет min
+    
+    foreach (double el in array) {
+        if (el > result[0]) result[0] = el;
+        if (el < result[1]) result[1] = el;
+    }
+    return result;
+}
+
+void PrintEmptyString() {
+    System.Console.WriteLine();
+}
